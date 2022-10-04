ee75dcdb0342737a24a2b99d30f2b4986c7ce3cf
ham222 <86384726+ham222@users.noreply.github.com>
2022-10-04 12:40:28 +0200

    Initial commit, spring boot app

---
+++ src/main/java/com/example/sdj3_assignment2/Sdj3Assignment2Application.java
@@ -0,0 +1,13 @@
+package com.example.sdj3_assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdj3Assignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdj3Assignment2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdj3_assignment2/Sdj3Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdj3_assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdj3Assignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
